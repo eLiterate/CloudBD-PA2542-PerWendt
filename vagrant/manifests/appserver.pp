338727b47147ef8dccd7e1f4f1467662947ae5e3
eLiterare <per@fam-wendt.se>
2018-10-17 00:03:37 +0200

    assignment 1

---
+++ vagrant/manifests/appserver.pp
@@ -0,0 +1,30 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+exec { 'add-nodesource-repo':
+	command => "curl -sL https://deb.nodesource.com/setup | sudo bash -",
+}
+exec { 'add-ubuntu-git-maintainers-repo':
+	command => "add-apt-repository ppa:git-core/ppa",
+}
+
+class system-update {
+	exec { 'apt-get update':
+		command => 'apt-get update',
+  }
+}
+
+class system-upgrade {
+	exec { 'apt-get upgrade':
+        	command => 'apt-get upgrade -y',
+    }
+}
+
+class js_packages {
+	package { "nodejs":
+    		ensure => "latest"
+    }
+}
+
+include system-update
+include system-upgrade
+include js_packages
